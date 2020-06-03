4c5c5a3b5e14314b97ca06f91340bd5f08d5e215
Aaron Campbell <sharkbeardio@gmail.com>
2020-06-03 00:55:15 -0400

    Add new css classes to views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+<div class="container bg-white">
+  <div class="py3">
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
         </div>
-    </div>
+    @endif
+    <h1>Welcome to Moodly!</h1>
+    <p class="lead">Share your feelings with the world and know you aren't alone.</p>
+  </div>
+  <div class="col-12 text-center">
+    <entry></entry>
+  </div>
 </div>
 @endsection
